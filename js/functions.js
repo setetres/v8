4cdbae851403d602a1219c39400ae06c2b530381
unknown <guilherme.pangnotta@JCDWEB07.JChebly.local>
2015-10-09 16:48:21 -0300

    Fix chars

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
 
+    var letter = document.getElementById("letter");
+    var cyclical = true;
+
+    function say(string, index){
+        if(index < string.length){
+            setTimeout(function(){
+                letter.innerHTML = string[index];
+                say(string,++index);
+            },100);
+            } else if(cyclical){
+            say(string, 0);
+        }
+    }
+    say("☂☔☂☔☂☔☂☔☂☔☐☒☐☒☐☒☐☑☐☑☐☑♔♚♕♛♖♜♘♞☰☱☲☴☰☱☳☷☶☴☰☴☶☷☳☱☰☴☲☵☲☵☲☰☷☰☷✢✣✤✥✽✻✼❋❊❉✺✹✸✷✶✦➝➞➟➠➩➪➯➱➩➪➯➱",0);
+
     $('.fit-video').fitVids();
 
     $(window).on('debouncedresize',function(event){
