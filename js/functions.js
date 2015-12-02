e40e35c8cc0e654d338194d601f652332b363a25
Guilherme Pangnotta <setetres@gmail.com>
2015-12-02 17:42:58 -0200

    Fix

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+    var backgroundAudio=document.getElementsByTagName("audio");
+    backgroundAudio.volume=0.2;
+
     window.sr = new scrollReveal({
         easing: 'cubic-bezier(0.19, 1, 0.22, 1)',
         over:   '0.3s',
@@ -8,9 +11,22 @@ $(document).ready(function(){
         viewport: document.getElementById('mirror')
     });
 
+    // hover
+
+    $(".project-list > li").each(function(i) {
+        if (i != 0) {
+            $("#hover").clone().attr("id", "hover" + i).appendTo($(this).parent());
+        }
+        $(this).data("hover", i);
+    }).mouseenter(function() {
+        $("#hover" + $(this).data("hover"))[0].play();
+    });
+    $("#hover").attr("id", "hover0");
+
+    // drag!
+
     $(function() {
         new Dragdealer('drag', {
-            x: window.location.hash == '#about' ? 1 : 0,
             steps: 2,
             animationCallback: function(x) {
                 $('.bar .slides').css('margin-left', (-x * 100) + '%');
